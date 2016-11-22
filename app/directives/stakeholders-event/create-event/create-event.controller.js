6dc30eae583be4b12c93cd24877a734185075726
Anna Gromych <agro@softserveinc.com>
2016-11-22 13:02:56 +0200

    NPS: implement multiselect for stakeholders event

--- app/directives/stakeholders-event/create-event/create-event.controller.js
+++ app/directives/stakeholders-event/create-event/create-event.controller.js
@@ -1,5 +1,5 @@
 export default class EventCreateCtrl {
-    constructor(dataStorage){
+    constructor(dataStorage) {
         this.dataStorage = dataStorage;
         this.event = this.getDefaultValue();
     }
@@ -12,14 +12,21 @@ export default class EventCreateCtrl {
         return {
             date: '18.11.2016',
             score: 0,
-            stakeholders: [
-                {id: ''}
-            ]
+            stakeholders: []
         };
     }
     createEvent () {
+        let stakeholdersOnEvent = this.event.stakeholders.toString().split('},');
+        for (let iterator = 0; iterator < stakeholdersOnEvent.length - 1; iterator++) {
+            stakeholdersOnEvent[iterator] = stakeholdersOnEvent[iterator] + "}";
+        }
+        for (let iterator = 0; iterator < stakeholdersOnEvent.length; iterator++) {
+            stakeholdersOnEvent[iterator] = JSON.parse(stakeholdersOnEvent[iterator]);
+            console.log(stakeholdersOnEvent[iterator]);
+        }
+        this.event.stakeholders = stakeholdersOnEvent;
         this.dataStorage.events.push(this.event);
         this.dataStorage.storeData(this.dataStorage.events, 'events');
-        this.stakeholder = this.getDefaultValue();
+        this.event = this.getDefaultValue();
     }
 }
--- app/directives/stakeholders-event/events-list/events-list.controller.js
+++ app/directives/stakeholders-event/events-list/events-list.controller.js
@@ -1,6 +1,5 @@
 export default class EventListCtrl {
     constructor(dataStorage){
         this.dataStorage = dataStorage;
-        //this.selectedEvent = {};
     }
 }
