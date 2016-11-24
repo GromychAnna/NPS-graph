7442c516eda56331695950a6a3cf2d07062cac1f
Anna Gromych <agro@softserveinc.com>
2016-11-24 19:40:50 +0200

    NPS: add firebase (need to solve error with permision)

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 //const ngModule = angular.module('app',['firebase']); // берём с html ангуляровский модуль и присваиваем переменной, чтоб можно было его передавать и с ним работать дальше
-const ngModule = angular.module('app',[]); // берём с html ангуляровский модуль и присваиваем переменной, чтоб можно было его передавать и с ним работать дальше
+const ngModule = angular.module('app',['firebase']); // берём с html ангуляровский модуль и присваиваем переменной, чтоб можно было его передавать и с ним работать дальше
 
 require('./directives')(ngModule); //говорим, что всё, что лежит в папке directives - подключим сюда
 require('./factories')(ngModule);
