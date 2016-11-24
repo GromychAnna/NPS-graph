5e643a31a52a2372ec19c586aa0fb82211837676
Anna Gromych <agro@softserveinc.com>
2016-11-24 21:43:46 +0200

    NPS: paint arrow if edge type is 'reports to'

--- app/directives/stakeholders-graph/stakeholders-graph.directive.js
+++ app/directives/stakeholders-graph/stakeholders-graph.directive.js
@@ -17,29 +17,83 @@ export default ngModule => {
                 ctx.fillStyle = "white";                                                                //белым цветом
                 ctx.fillRect(0,0, canvas.width, canvas.height);                                         //закрашиваем всю область
 
-                particleSystem.eachEdge(                                                                //отрисуем каждую грань
-                    function(edge, pt1, pt2){                                                           //будем работать с гранями и точками её начала и конца где edge: {source:Node, target:Node, length:#, data:{}}//console.warn('EDGE', data);
-                        ctx.strokeStyle = "rgba(0,0,0, .333)";                                          //грани будут чёрным цветом с некой прозрачностью
-                        ctx.lineWidth = 1;
-                        ctx.beginPath();//начинаем рисовать
-                        ctx.moveTo(pt1.x, pt1.y);//от точки один
-                        ctx.lineTo(pt2.x, pt2.y);//до точки два
-                        ctx.stroke();
-                    });
+
+                let nodeBoxes = {};
 
                 particleSystem.eachNode(                                                                //теперь каждую вершину
                     (node, pt) => {                                                                 //получаем вершину и точку где она где node: {mass:#, p:{x,y}, name:"", data:{}}
                         const w = 13;                                                                     //ширина квадрата //ctx.fillStyle = (node.data.alone) ? "orange" : "black";
-                        ctx.fillStyle = node.data.score > 0 ? "#32CD32" : "#DC143C";
+                        ctx.fillStyle = node.data.score > 0 ? "#2E8B57" : "#DC143C";
                         if (node.data.score == 0) {
                             ctx.fillStyle = "#696969";
                         }
+                        nodeBoxes[node.name] = [pt.x-w/2, pt.y-11, w, 22];
                         ctx.fillRect(pt.x-w/2, pt.y-w/2, w,w);                                          //рисуем
                         ctx.fillStyle = "black";                                                        //цвет для шрифта
 
                         ctx.font = 'italic 13px sans-serif';                                            //шрифт
                         ctx.fillText (`${node.data.firstName}  ${node.data.score}`, pt.x+8, pt.y+8);                             //пишем имя у каждой точки
-                    })
+                    });
+
+                particleSystem.eachEdge(                                                                //отрисуем каждую грань
+                    function(edge, pt1, pt2){                                                           //будем работать с гранями и точками её начала и конца где edge: {source:Node, target:Node, length:#, data:{}}//console.warn('EDGE', data);
+
+                        //ctx.strokeStyle = "rgba(0,0,0, .333)";                                          //грани будут чёрным цветом с некой прозрачностью
+                        //ctx.lineWidth = 1;
+                        //ctx.beginPath();//начинаем рисовать
+                        //ctx.moveTo(pt1.x, pt1.y);//от точки один
+                        //ctx.lineTo(pt2.x, pt2.y);//до точки два
+                        //ctx.stroke();
+
+                        const weight = 1;
+                        let color = "#DCDCDC";
+
+                        //if (!color || (` ${color}`).match(/^[ \t]*$/)) { color = null};
+
+                        // find the start point
+                        let tail = intersect_line_box(pt1, pt2, nodeBoxes[edge.source.name]);
+                        let head = intersect_line_box(tail, pt2, nodeBoxes[edge.target.name]);
+
+                        ctx.save();
+                        ctx.beginPath();
+                        ctx.lineWidth = (!isNaN(weight)) ? parseFloat(weight) : 1;
+                        ctx.strokeStyle = (color) ? color : "#cccccc";
+                        ctx.fillStyle = null;
+
+                        ctx.moveTo(tail.x, tail.y);
+                        ctx.lineTo(head.x, head.y);
+                        ctx.stroke();
+                        ctx.restore();
+
+                        // draw an arrowhead if this is a -> style edge
+                        if (edge.data === "Reports to"){
+                            ctx.save();
+                            let color = "#9BCD9B";
+                            // move to the head position of the edge we just drew
+                            let wt = !isNaN(weight) ? parseFloat(weight) : 1;
+                            let arrowLength = 16 + wt;
+                            let arrowWidth = 4 + wt;
+                            ctx.fillStyle = color;
+                            ctx.strokeStyle = "#9BCD9B";
+                            ctx.translate(head.x, head.y);
+                            ctx.rotate(Math.atan2(head.y - tail.y, head.x - tail.x));
+
+                            // delete some of the edge that's already there (so the point isn't hidden)
+                            ctx.clearRect(-arrowLength/2,-wt/2, arrowLength/2,wt);
+
+                            // draw the chevron
+                            ctx.beginPath();
+                            ctx.moveTo(-arrowLength, arrowWidth);
+                            ctx.lineTo(0, 0);
+                            ctx.lineTo(-arrowLength, -arrowWidth);
+                            ctx.lineTo(-arrowLength * 0.8, -0);
+                            ctx.closePath();
+                            ctx.fill();
+                            ctx.restore()
+                        }
+                    });
+
+
             },
 
             initMouseHandling: () => {                                                               //события с мышью no-nonsense drag and drop (thanks springy.js)
@@ -135,6 +189,34 @@ export default ngModule => {
                 canvas.addEventListener("mousedown", handler.clicked);
             }
         };
+        // helpers for figuring out where to draw arrows (thanks springy.js)
+        function intersect_line_line  (p1, p2, p3, p4) {
+            let denom = ((p4.y - p3.y)*(p2.x - p1.x) - (p4.x - p3.x)*(p2.y - p1.y));
+            if (denom === 0) return false; // lines are parallel
+            let ua = ((p4.x - p3.x)*(p1.y - p3.y) - (p4.y - p3.y)*(p1.x - p3.x)) / denom;
+            let ub = ((p2.x - p1.x)*(p1.y - p3.y) - (p2.y - p1.y)*(p1.x - p3.x)) / denom;
+
+            if (ua < 0 || ua > 1 || ub < 0 || ub > 1)  return false;
+            return arbor.Point(p1.x + ua * (p2.x - p1.x), p1.y + ua * (p2.y - p1.y));
+        }
+
+        function intersect_line_box (p1, p2, boxTuple) {
+            let p3 = {x:boxTuple[0], y:boxTuple[1]},
+                w = boxTuple[2],
+                h = boxTuple[3];
+
+            let tl = {x: p3.x, y: p3.y};
+            let tr = {x: p3.x + w, y: p3.y};
+            let bl = {x: p3.x, y: p3.y + h};
+            let br = {x: p3.x + w, y: p3.y + h};
+
+            return intersect_line_line(p1, p2, tl, tr) ||
+                intersect_line_line(p1, p2, tr, br) ||
+                intersect_line_line(p1, p2, br, bl) ||
+                intersect_line_line(p1, p2, bl, tl) ||
+                false
+        }
+
         return that;
     }
 
@@ -159,7 +241,7 @@ export default ngModule => {
         });
 
         _.forEach(edges, (edge) => {
-            sys.addEdge(edge.src,edge.dest);                                                            //добавляем грань
+            sys.addEdge(edge.src,edge.dest,edge.edgeType);                                                            //добавляем грань
         });
 
     }
